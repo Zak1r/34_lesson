e02c90f28761380e180f6d01f466bf70fc6de2ac
Zak1r <94119532+Zak1r@users.noreply.github.com>
2022-03-11 21:31:24 +0600

    end

---
+++ inde.py
@@ -0,0 +1,30 @@
+# Позиционные системы счисления
+
+
+# абстракция это
+
+
+# print( (-2) ** 0.5)
+
+# import math
+
+# print(math.sqrt(-2))
+
+a = 5 + 6j
+b = 2 + 3j
+
+# print(type(a))
+# print(a.real) # 
+# print(a.imag) # мнимое число
+
+# print(a + b)
+
+print(1 + 2)
+print(bin(0b10 + 0b11))
+print(0o111 + 0b11)
+print(oct(0o111 + 0b11))
+print(hex(0xFF * 2))
+print(int('0xZZZZ', 36))
+print(int('Y', 35))
+
+
